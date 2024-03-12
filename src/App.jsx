71e52f4c7b22988d2b3a95ab58a2c9f2ae82d7df
asselzhl <asselzhly@gmail.com>
2024-03-12 22:48:18 +0500

    build layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React from "react"
+import { FaPlus } from "react-icons/fa";
 
+const style = {
+  bg: `h-screen w-screen bg-[#0F103F] p-4`,
+  container: `bg-transparent-blue max-w-[700px] w-full m-auto rounded-md p-4`,
+  title: `text-white text-3xl font-bold text-center p-2 mb-5`,
+  form: `flex justify-between`,
+  input: `w-full p-2 text-xl border`,
+  button: `ml-3 border p-4 bg-white`
+}
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h2 className={style.title}>TODO App</h2>
+        <form className={style.form}>
+          <input className={style.input} placeholder="Add TODO" type="text" />
+          <button className={style.button}><FaPlus size={30} /></button>
+        </form>
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "transparent-blue": "rgba(47, 47, 138, 0.60)",
+        "transparent-gray": "rgba(255, 255, 255, 0.60)",
+        "transparent-white": "rgba(255, 255, 255, 0.60)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
